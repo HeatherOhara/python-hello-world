b7046a1709b5e1052f0d01f5cde53976ebbb874a
Heather <heather.fields03@gmail.com>
2024-08-14 20:43:31 -0500

    Helloe Gorgeous

--- hello.py
+++ hello.py
@@ -3,7 +3,17 @@
 # Enter your code on line 7 to print 'Hello, World!'
 # to the console (the panel on the right)
 
-print("Hello");
+print("Hello")
+print("hello gorgeous")
+print ('Hello Gorgeous')
+
+print (2 +2)
+print("I've got a lovely bunch of coconuts","there they are a standing in a row")
+print ("quoth the raven, 'Nevermore'")
+
+X = 1 
+y = 1/2
+print( X+y)
 
 # Next, PLAY!
 # Modify your code on line 7 to try to accomplish the tasks listed below:
@@ -13,7 +23,7 @@ print("Hello");
 #   3. Remove one or both quotation marks. Do we need to include both opening and closing quote marks? Is there a difference between using a single or a double quote (' vs. ")?
 #   4. Print a number. (Bonus: Print two numbers added together).
 #   5. Print multiple messages one after the other.
-#   6. Print two messages on the same line.
+#   6. Print two mespsages on the same line.
 #   7. Print a message that contains quote marks, such as Quoth the Raven "Nevermore".
 #   8. Other. You choose!
 #
